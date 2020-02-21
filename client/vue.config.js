1789bd6028b247aa7668fcd060d7531c07fb7545
Zombie <prodijal@gmail.com>
2020-02-21 20:55:26 +0800

    change host ip

--- client/vue.config.js
+++ client/vue.config.js
@@ -44,7 +44,7 @@ module.exports = {
         hotOnly: false,
         proxy: { // 配置跨域
             '/api': {
-                target: 'http://localhost:5000/api/',
+                target: 'http://0.0.0.0:5000/api/',
                 ws: true,
                 changOrigin: true,
                 pathRewrite: {
